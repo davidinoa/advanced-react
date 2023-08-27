bc6d880f84a3ea3526c0652795c69fd62cb1b258
David Inoa <davidinoa@gmail.com>
2023-08-27 12:19:40 -0400

    Add ProductImage schema

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 import { Product } from './schemas/Product';
 import { User } from './schemas/User';
 import 'dotenv/config';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -43,6 +44,7 @@ export default withAuth(
       // Schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // Show the UI only for poeple who pass this test
