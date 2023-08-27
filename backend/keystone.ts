4c8ab164d71ccbf7469b06b8f8c3058fbeb5f5e7
David Inoa <davidinoa@gmail.com>
2023-08-27 11:31:08 -0400

    Create Product data type

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -4,6 +4,7 @@ import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { Product } from './schemas/Product';
 import { User } from './schemas/User';
 import 'dotenv/config';
 
@@ -41,6 +42,7 @@ export default withAuth(
     lists: createSchema({
       // Schema items go in here
       User,
+      Product,
     }),
     ui: {
       // Show the UI only for poeple who pass this test
