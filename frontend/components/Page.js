6a3c83b8dc11e93df5d27b8cd1f30de3feda9d1d
David Inoa <davidinoa@gmail.com>
2023-09-03 17:44:00 -0400

    Implement Products and Product components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -20,6 +20,7 @@ const GlobalStyles = createGlobalStyle`
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
     box-sizing: border-box;
+    font-size: 62.5%;
   }
 
   *, *:before, *:after {
@@ -31,7 +32,7 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     margin: 0;
     font-size: 1.5rem;
-    line-height:2;
+    line-height: 2;
   }
 
   a {
