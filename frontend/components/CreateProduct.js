41f098102fbb91714b548a90c476608ad65fc54d
David Inoa <davidinoa@gmail.com>
2023-09-09 15:31:39 -0400

    Implement mutation for creating products

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,32 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
 import useForm from "../lib/useForm";
+import DisplayError from "./ErrorMessage";
 import Form from "./styles/Form";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,14 +36,24 @@ export default function CreateProduct() {
     description: "These are the best shoes!",
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
@@ -59,7 +96,6 @@ export default function CreateProduct() {
             onChange={handleChange}
           />
         </label>
-
         <button type="submit">+ Add Product</button>
       </fieldset>
     </Form>
