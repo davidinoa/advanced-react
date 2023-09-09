525d23c9712fb7ddd90e5802e2a61bae2820c9a0
David Inoa <davidinoa@gmail.com>
2023-09-09 16:19:18 -0400

    Refetch products query after successful mutation

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import useForm from "../lib/useForm";
 import DisplayError from "./ErrorMessage";
 import Form from "./styles/Form";
+import { ALL_PRODUCTS_QUERY } from "./Products";
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -40,6 +41,7 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ ALL_PRODUCTS_QUERY }],
     }
   );
 
--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,9 +1,9 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
-import Product from './Product';
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Product from "./Product";
 
-const ALL_PRODUCTS_QUERY = gql`
+export const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
     allProducts {
       id
